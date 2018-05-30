21b8e6b27035f9dd309084d005eaa96562e6c408
spiritaufe <34106375+spiritaufe@users.noreply.github.com>
2018-05-30 16:18:38 +0300

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "socialinfluencecoin")
-set(CN_CURRENCY_DISPLAY_NAME "Social Influence Coin")
-set(CN_CURRENCY_TICKER "SIC")
+set(CN_PROJECT_NAME "photoncoin")
+set(CN_CURRENCY_DISPLAY_NAME "PhotonCoin")
+set(CN_CURRENCY_TICKER "PHC")
